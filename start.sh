f8a4a43c285c9ab84138ca366aa215f470097008
dihamon <brain.entity@gmail.com>
2015-10-18 18:23:59 +0200

    Sun Oct 18 18:23:59 CEST 2015

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ function user ()
     #cp /arch/conf/.xinitrc /arch/conf/.xbindkeysrc /arch/conf/.yaourtrc /home/"$USER"/ &> .log || return 1
     #cat arch/conf/bashrc >> home/"${USER}"/.bashrc || return 1
     #echo -e "tmp\ntmp\n" | passwd "$USER" &> .log || return 1
-    #echo "$USER ALL=(ALL) ALL" >> /etc/sudoers || return 1
+    echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers || return 1
     return 0
 }
 
@@ -40,16 +40,18 @@ function list ()
 	   )
     export PACKAGELIST=$(
 	echo $PACKAGELIST | sed -e "\
-s/1/\/arch\/openbox-additionals-packagelist/;\
-s/2/\/arch\/cinnamon-additionals-packagelist/;\
+s/1/\/arch\/openbox-additions-packagelist/;\
+s/2/\/arch\/cinnamon-additions-packagelist/;\
 s/3/\/arch\/software-base-packagelist/;\
 s/4/\/arch\/media-additions-packagelist/;\
 " 2> .log)
-    [ -z "$PACKAGELIST" ] && export PACKAGELIST="xmonad"
-    su "$USER" -c "echo -e \"\n${PASS}\n\" \
-		| yaourt -S \$(sed -e \"s/#.*//g;s/[ \t]*$//g\" $PACKAGELIST) \
-			 --noconfirm" &> .log || return 1
-    #su "$USER" -c "echo -e \"\n${PASS}\n\" | yaourt -Rcns \$(yaourt -Qqdt)" &> .log || return 1
+    if [ -z "$PACKAGELIST" ]; then
+	export PACKAGELIST="xmonad"
+    else
+	export PACKAGELIST="$(sed -e "s/#.*//g;s/[ \t]*$//g" $PACKAGELIST 2> .log)"; fi
+    su "$USER" -c "yaourt -S $PACKAGELIST --noconfirm" &> .log || return 1
+    echo | su "$USER" -c \
+	      "yaourt -Rcns \$(yaourt -Qqdt)" &> .log
     return 0
 }
 
@@ -67,18 +69,37 @@ function run ()
 
 function deps ()
 {
+    sed -i "s/$USER ALL=(ALL) NOPASSWD:ALL/$USER ALL=(ALL) ALL/" /etc/sudoers || return 1
+    if [ -e /bin/openbox ];
+    then
+	cp -r /arch/conf/openbox /home/${USER}/.config &> .log || return 1
+	cp -r /arch/conf/lxterminal /home/${USER}/.config &> .log || return 1
+	cp -r /arch/conf/gtk-3.0 /home/${USER}/.config &> .log || return 1
+	cp -r /arch/conf/tint2 /home/${USER}/.config &> .log || return 1
+	rm /usr/share/xsession/openbox-kde.desktop &> .log || return 1
+    fi
+    return 0
+}
+
+function passroot ()
+{
+    PASSROOT=$(whiptail --title "First start configuration" \
+			--passwordbox "Enter your root password here : " \
+			20 90 3>&1 1>&2 2>&3)
+    [ -z "$PASSROOT" ] && return 1
+    echo -e "${PASSROOT}\n${PASSROOT}\n" | passwd &> .log || return 1
     return 0
 }
 
 run user
-#
 run pass
 run list
-#exit
 run deps
+run passroot
 
 echo $USER
 echo $PASS
 echo $PACKAGELIST
 
+#systemctl enable slim
 #rm -rfv /etc/systemd/system/getty.target.wants/getty@tty1.service /etc/systemd/system/autologin@.service
